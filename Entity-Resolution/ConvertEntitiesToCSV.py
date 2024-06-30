5cdadb57388e4b77219699249eb738c8bc4b3359
BaliBot2 <99399589+BaliBot2@users.noreply.github.com>
2024-06-30 12:01:07 +0530

    Converts Entities from ES index to a CSV

---
+++ Entity-Resolution/ConvertEntitiesToCSV.py
@@ -0,0 +1,76 @@
+import csv
+from elasticsearch import Elasticsearch, helpers
+
+client = Elasticsearch(
+    cloud_id="5a253761e0f2466baedd513681b7723e:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQwY2ZkZDVjZDNmZGQ0NzNjYmJiZjYzNzdkYjE3YTI1NyRkOWNlY2IwMDAyM2M0NGJjYWUzYWY2NjEyODczMThjNQ==", 
+    api_key="NklNR0xKQUJ6VWRlaTd2RWgycTA6T1dQR0lnTjlSSHlMelRzNl9FNDVoQQ==",
+    timeout=30,
+    retry_on_timeout=True,
+    max_retries=5
+)
+
+def fetch_all_documents(index_name):
+    query = {
+        "size": 10000,  
+        "query": {
+            "nested": {
+                "path": "aliases",
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "exists": {
+                                    "field": "aliases.ID"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+    response = client.search(index=index_name, body=query, scroll='2m')
+    scroll_id = response['_scroll_id']
+    hits = response['hits']['hits']
+    
+    all_documents = hits
+    
+    while len(hits) > 0:
+        response = client.scroll(scroll_id=scroll_id, scroll='2m')
+        hits = response['hits']['hits']
+        all_documents.extend(hits)
+    
+    return all_documents
+
+def process_documents(documents):
+    rows = []
+    for doc in documents:
+        source = doc['_source']
+        entity = f"{source['Name']}({source['title']},{source['ID']})"
+        
+        aliases = source.get('aliases', [])
+        sorted_aliases = sorted(aliases, key=lambda x: x['confidence'], reverse=True)
+        aliases_str = ", ".join([f"{alias['Name']}({alias['title']},{alias['ID']},{alias['confidence']})" for alias in sorted_aliases])
+        
+        rows.append([entity, aliases_str])
+    
+    return rows
+
+def save_to_csv(rows1, filename='Meta3.csv'):
+    header = ['Entities', 'Aliases']
+    
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(rows1)
+
+
+def main():
+    index_name1 = 'resolved_entities'    
+    documents1 = fetch_all_documents(index_name1)
+    rows1 = process_documents(documents1)
+    
+    save_to_csv(rows1)
+
+if __name__ == "__main__":
+    main()
