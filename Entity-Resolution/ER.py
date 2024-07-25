f669c4c332dbda4f396e1bd3c4338600eb98faab
Anurav Singh <122572857+anurav088@users.noreply.github.com>
2024-07-25 22:30:56 +0530

    Update ER.py

--- Entity-Resolution/ER.py
+++ Entity-Resolution/ER.py
@@ -6,8 +6,8 @@ from elasticsearch import Elasticsearch
 from collections import Counter
 
 client = Elasticsearch(
-    cloud_id="5a253761e0f2466baedd513681b7723e:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQwY2ZkZDVjZDNmZGQ0NzNjYmJiZjYzNzdkYjE3YTI1NyRkOWNlY2IwMDAyM2M0NGJjYWUzYWY2NjEyODczMThjNQ==", 
-    api_key="NklNR0xKQUJ6VWRlaTd2RWgycTA6T1dQR0lnTjlSSHlMelRzNl9FNDVoQQ==",
+    cloud_id="ES_CLOUD_ID", 
+    api_key="ES_API_KEY",
     timeout=30,
     retry_on_timeout=True,
     max_retries=5)
